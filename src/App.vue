998afcfd4ce8eacb406df1cb0e36e5ec1ed82e14
monikamikec03 <50955164+monikamikec03@users.noreply.github.com>
2019-11-15 18:06:05 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Novosti</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Događanja
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Natjecanja</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Kontakt</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Login (članovi)</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+<!--Novosti-->
+<Novosti/>
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+  </div>
+</template>
+
+<script>
+
+import Novosti from './components/Novosti.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Novosti
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Novosti.vue
@@ -0,0 +1,75 @@
+<template>
+  <!-- Page Content -->
+  <div class="container">
+
+    <!-- Jumbotron Header -->
+    <header class="jumbotron my-4">
+      <h1 class="display-3">A Warm Welcome!</h1>
+      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
+      <a href="#" class="btn btn-primary btn-lg">Call to action!</a>
+    </header>
+
+    <!-- Page Features -->
+    <div class="row text-center">
+      <div class="col-lg-3 col-md-6 mb-4" v-for="(obj, key) in novostiJSON" :key="key">
+        <div class="card h-100">
+          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+          <div class="card-body">
+            <h4>{{ obj.caption }}</h4>
+            <p class="card-text">{{ obj.paragraph }}</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">{{ obj.readMore }}</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+</template>
+
+<script>
+export default{
+    data: () => ({
+        novostiJSON: [
+            {
+                img: '/something/random/img.jpg',
+                title: 'Natjecanje u Ostrni', 
+                paragraph: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.',
+                readMore: 'Pročitaj više!' 
+            },
+            {
+                img: '/something/random/img.jpg',
+                title: 'Godišnja skupština', 
+                paragraph: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.',
+                readMore: 'Pročitaj više!' 
+            },
+            {
+                img: '/something/random/img.jpg',
+                title: '16. Memorijal bana Josipa Jelačića', 
+                paragraph: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.',
+                readMore: 'Pročitaj više!' 
+            },
+            {
+                img: '/something/random/img.jpg',
+                title: 'Dva nova vatrogasca s posebnim ovlastima', 
+                paragraph: 'Neki tekst',
+                readMore: 'Pročitaj više!' 
+            },
+            {
+                img: '/something/random/img.jpg',
+                title: 'Nanovo dodani članak', 
+                paragraph: 'Ovo sam sama naknadno unijela',
+                readMore: 'Pročitaj više!' 
+            }
+        ]
+    })
+
+}
+</script>
+
+<style lang="css">
+
+</style>
