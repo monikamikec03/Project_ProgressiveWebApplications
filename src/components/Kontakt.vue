7f52846141f21d6ef8e72ae6bec21b8340a89813
monikamikec03 <50955164+monikamikec03@users.noreply.github.com>
2019-11-16 12:14:51 +0100

    SS

--- src/components/Kontakt.vue
+++ src/components/Kontakt.vue
@@ -1,5 +1,9 @@
 <template>
-    <h1 class="display-5">Kontakt ovdje </h1>
+  <div class="container">
+        <header class="jumbotron my-4">
+            <h1 class="display-5">Kontakt ovdje </h1>
+        </header>
+  </div>
 </template>
 
 <script>
