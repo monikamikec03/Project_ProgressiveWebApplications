2f7ac4f6c77c7d94e67730b9c93e9c7eb48bfc07
monikamikec03 <50955164+monikamikec03@users.noreply.github.com>
2019-11-16 12:09:58 +0100

    Descr

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-    <!--<img src="../assets/logo.png" style="width:4%"/>-->
+    <img class="card-header-pills" src="../assets/logo.png" style="width:4%"/>
       <a class="navbar-brand"><router-link to="/">DVD Haganj</router-link></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" >
         <span class="navbar-toggler-icon" v-onclick="toggleNav"></span>
