016e2f20fcbe6c5721998d79dd484892582ed3e3
monikamikec03 <50955164+monikamikec03@users.noreply.github.com>
2020-06-06 09:19:40 +0200

    Update Novosti.vue

--- src/components/Novosti.vue
+++ src/components/Novosti.vue
@@ -1,89 +1,114 @@
 <template>
   <!-- Page Content -->
   <div class="container">
-
     <!-- Jumbotron Header -->
     <header class="jumbotron my-4">
       <h1 class="display-5 py-5">Dobrodošli na stranicu Dobrovoljnog vatrogasnog društva Haganj</h1>
-      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
-      <a href="https://www.facebook.com/Dvd-Haganj-145464629144060/" class="btn btn-primary btn-lg" target="_blank">Posjeti facebook stranicu!</a>
-    <div v-for="(obj,key) in slike" :key="key">
+      <p
+        class="lead"
+      >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
+      <a
+        href="https://www.facebook.com/Dvd-Haganj-145464629144060/"
+        class="btn btn-primary btn-lg"
+        target="_blank"
+      >Posjeti facebook stranicu!</a>
+      <div v-for="(obj,key) in slike" :key="key">
+        <ul>
+          <li v-for="(data, index) in slike" :key="index" @click="otvoriSliku(data.slika)">
+            <img class="card-img-top" alt :src="data.slika">
+          </li>
+        </ul>
+      </div>
 
-            <ul>
-                <li v-for="(data, index) in slike" :key="index">
-                              <img class="card-img-top" alt="" :src="data.slika">
-                </li>
-            </ul>
-    </div>
+      <div class="modal" v-if="curImg">
+        <button class="modal-close" @click="otvoriSliku(null)">Zatvori</button>
+        <img class="card-img-top" alt :src="curImg">
+      </div>
     </header>
   </div>
 </template>
 
 <script>
-export default{
-data: () => ({
-  slike: [
-    {
-      slika: "https://scontent.fzag2-1.fna.fbcdn.net/v/t1.0-9/s960x960/39454208_677875732569611_420377886965366784_o.jpg?_nc_cat=110&_nc_oc=AQlaMBjqNUQKXdrkQ4Lom_6IiUJqsBwKG1DP2agv74wsJwpPjWy0ucfSoKi0Vb_sB58&_nc_ht=scontent.fzag2-1.fna&_nc_tp=7&oh=f93db63e80a481d27a0da181da643f4b&oe=5F021CCD"
-    },
-    {
-      slika: "https://scontent.fzag2-1.fna.fbcdn.net/v/t31.0-8/s960x960/28516780_563935500630302_4453077750382845337_o.jpg?_nc_cat=102&_nc_oc=AQkQYVdIEWZJvvLpVVOZY_36Olu1_8PtUZMXgyWdZ4n4pxlGl6_9oGKC383-C9TyJOE&_nc_ht=scontent.fzag2-1.fna&_nc_tp=7&oh=985ced84c8ba27e07824f483db9c4979&oe=5EC20D0D",
-
-    },
-    {
-      slika: "https://scontent.fzag2-1.fna.fbcdn.net/v/t1.0-9/s960x960/39454208_677875732569611_420377886965366784_o.jpg?_nc_cat=110&_nc_oc=AQlaMBjqNUQKXdrkQ4Lom_6IiUJqsBwKG1DP2agv74wsJwpPjWy0ucfSoKi0Vb_sB58&_nc_ht=scontent.fzag2-1.fna&_nc_tp=7&oh=f93db63e80a481d27a0da181da643f4b&oe=5F021CCD"
-    },
-    {
-      slika: "https://scontent.fzag2-1.fna.fbcdn.net/v/t31.0-8/s960x960/28516780_563935500630302_4453077750382845337_o.jpg?_nc_cat=102&_nc_oc=AQkQYVdIEWZJvvLpVVOZY_36Olu1_8PtUZMXgyWdZ4n4pxlGl6_9oGKC383-C9TyJOE&_nc_ht=scontent.fzag2-1.fna&_nc_tp=7&oh=985ced84c8ba27e07824f483db9c4979&oe=5EC20D0D",
-
-    },
-
-    
-
-  ]
-
-    })
-    
-}
+export default {
+  data: () => ({
+    slike: [
+      {
+        slika:
+          "https://scontent.fzag2-1.fna.fbcdn.net/v/t1.0-9/s960x960/39454208_677875732569611_420377886965366784_o.jpg?_nc_cat=110&_nc_oc=AQlaMBjqNUQKXdrkQ4Lom_6IiUJqsBwKG1DP2agv74wsJwpPjWy0ucfSoKi0Vb_sB58&_nc_ht=scontent.fzag2-1.fna&_nc_tp=7&oh=f93db63e80a481d27a0da181da643f4b&oe=5F021CCD"
+      },
+      {
+        slika:
+          "https://scontent.fzag2-1.fna.fbcdn.net/v/t31.0-8/s960x960/28516780_563935500630302_4453077750382845337_o.jpg?_nc_cat=102&_nc_oc=AQkQYVdIEWZJvvLpVVOZY_36Olu1_8PtUZMXgyWdZ4n4pxlGl6_9oGKC383-C9TyJOE&_nc_ht=scontent.fzag2-1.fna&_nc_tp=7&oh=985ced84c8ba27e07824f483db9c4979&oe=5EC20D0D"
+      },
+      {
+        slika:
+          "https://scontent.fzag2-1.fna.fbcdn.net/v/t1.0-9/s960x960/39454208_677875732569611_420377886965366784_o.jpg?_nc_cat=110&_nc_oc=AQlaMBjqNUQKXdrkQ4Lom_6IiUJqsBwKG1DP2agv74wsJwpPjWy0ucfSoKi0Vb_sB58&_nc_ht=scontent.fzag2-1.fna&_nc_tp=7&oh=f93db63e80a481d27a0da181da643f4b&oe=5F021CCD"
+      },
+      {
+        slika:
+          "https://scontent.fzag2-1.fna.fbcdn.net/v/t31.0-8/s960x960/28516780_563935500630302_4453077750382845337_o.jpg?_nc_cat=102&_nc_oc=AQkQYVdIEWZJvvLpVVOZY_36Olu1_8PtUZMXgyWdZ4n4pxlGl6_9oGKC383-C9TyJOE&_nc_ht=scontent.fzag2-1.fna&_nc_tp=7&oh=985ced84c8ba27e07824f483db9c4979&oe=5EC20D0D"
+      }
+    ],
+    curImg: null
+  }),
+  methods: {
+    otvoriSliku(url) {
+      this.curImg = url;
+    }
+  }
+};
 </script>
 
 <style scoped>
- ul{
-        list-style-type:none;
-        -webkit-column-count: 4;
-        -moz-column-count: 4;
-        column-count: 4;
-        font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-        margin-top: 30px;
+ul {
+  list-style-type: none;
+  -webkit-column-count: 4;
+  -moz-column-count: 4;
+  column-count: 4;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  margin-top: 30px;
+}
+#clanovi {
+  width: 100%;
+  margin: 10px;
+  margin-top: 100px;
+}
+li:hover {
+  font-size: 20px;
+  color: orange;
+  opacity: 1;
+}
+li {
+  padding-bottom: 5px;
+}
+img {
+  width: 100%;
+  z-index: -1;
+  opacity: 0.5;
+}
 
-        
+.modal {
+  position: fixed;
+  display: flex;
+  padding: 20px;
+}
 
-    }
-    #clanovi{
-        width:100%;
-        margin:10px;
-        margin-top:100px;
+.modal img {
+  margin: auto;
+  opacity: 1;
+  position: relative;
+  z-index: 10;
+}
 
-    }
-    li:hover{
-        font-size: 20px;
-        color:orange;
-        opacity: 1;
-
-    }
-    li{
-        padding-bottom:5px;
-    }
-    img:hover{
-      position: relative;
-      width:420%;
-      z-index: 1;
-      opacity: 1;
-    }
-
-img{
-  width:100%;
-  z-index:-1;
-  opacity:0.5;
+.modal-close {
+  all: unset;
+  position: absolute;
+  z-index: 0;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  
 }
 </style>
